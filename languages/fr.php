f56b6e6c95c5987ed16d421fba38a62537429738
ManUtopiK <emmanuel.salomon@free.fr>
2013-03-26 12:47:52 +0100

    Added french translation

--- languages/fr.php
+++ languages/fr.php
@@ -16,32 +16,32 @@ $french = array(
 	 * Menu items and titles
 	 */
 
-	'workflow' => "Workflow",
-	'my_workflow' => "My Board",
-	'workflow:owner' => "%s's workflow",
-	'workflow:board:all' => "All workflow boards",
-	'workflow:all' => "all",
-	'workflow:assigned-cards' => "Assigned cards",
-	'workflow:assigned-cards:all' => "All",
-	'workflow:assigned-cards:title:all' => "All assigned cards",
-	'workflow:assigned-cards:title:mine' => "My assigned cards",
-	'workflow:assigned-cards:title:owner' => "%s's assigned cards",
-	'workflow:assigned-cards:friends' => "Friends",
-	'workflow:assigned-cards:title:friends' => "Friends's Assigned cards",
+	'workflow' => "Flux de travail",
+	'my_workflow' => "Mon tableau",
+	'workflow:owner' => "Flux de travail de %s",
+	'workflow:board:all' => "Tous les tableaux",
+	'workflow:all' => "Tous les flux de travail",
+	'workflow:assigned-cards' => "Fiches assignées",
+	'workflow:assigned-cards:all' => "Toutes les fiches",
+	'workflow:assigned-cards:title:all' => "Toutes les fiches assignées",
+	'workflow:assigned-cards:title:mine' => "Mes fiches assignées",
+	'workflow:assigned-cards:title:owner' => "Les fiches assignées à %s",
+	'workflow:assigned-cards:friends' => "Mes abonnements",
+	'workflow:assigned-cards:title:friends' => "Les fiches assignés à mes abonnements",
 
-	'item:object:workflow_board' => "Workflow boards",
-	'item:object:workflow_list' => "Workflow lists",
-	'item:object:workflow_card' => "Workflow cards",
+	'item:object:workflow_board' => "Tableaux",
+	'item:object:workflow_list' => "Listes",
+	'item:object:workflow_card' => "Fiches",
 
-	'workflow:add' => "Add board",
+	'workflow:add' => "Ajouter un tableau",
 	'workflow:add_list' => "Ajouter une liste",
 	'workflow:refresh_board' => "Rafraîchir",
 
-	'workflow:group' => "Group workflows",
-	'workflow:board:none' => "No board created yet",
-	'workflow:board' => "Board %s",
-	'workflow:board:owner' => "%s's boards",
-	'workflow:board:add' => "Add new board",
+	'workflow:group' => "Flux de travail du groupe",
+	'workflow:board:none' => "Aucun tableau n'a été créé pour l'instant",
+	'workflow:board' => "Tableau \"%s\"",
+	'workflow:board:owner' => "Tableaux de %s",
+	'workflow:board:add' => "Ajouter un tableau",
 	'workflow:board:created_by' => "Créé par %s",
 	'workflow:board:info' => "Contient %s listes et %s fiches",
 	'workflow:board:last_action' => "Dernière action",
@@ -53,29 +53,30 @@ $french = array(
 	'workflow:list:delete:confirm' => "Êtes-vous sûr de supprimer cette liste et toutes ses fiches ?",
 	'workflow:card:delete:confirm' => "Êtes-vous sûr de supprimer cette fiche ?",
 	'workflow:card:none' => "Aucune fiche n'a été créée.",
-	
+	'deck_river:workflow:card-info-header' => "Informations sur la fiche",
+
 	'workflow:archive' => "Archives",
-	'workflow:board:archive' => "Archives of the board %s",
-	'workflow:board:back' => "Go back to the board",
+	'workflow:board:archive' => "Archives du tableau %s",
+	'workflow:board:back' => "Retourner au tableau",
 
 	/**
 	* River
 	**/
-	'river:inboard' => "in board %s",
+	'river:inboard' => "dans le tableau %s",
 	'river:in:workflow_list' => "dans la liste %s",
-	'river:create:object:workflow_card_list:summary' => "%s modified board %s %s",
+	'river:create:object:workflow_card_list:summary' => "%s a modifié le tableau %s %s",
 	'river:create:object:workflow_list:message' => "A ajouté la liste %s",
 	'river:create:object:workflow_card:message' => "A ajouté la fiche %s %s",
 	'river:comment:object:workflow_card' => "%s commenté la fiche %s",
 	'workflow:card:shorted:message' => "+%s actions",
-	'river:create:object:workflow_board' => "%s added board %s",
+	'river:create:object:workflow_board' => "%s a ajouté le tableau %s",
 	'river:create:object:workflow_card:move:message' => "A déplacé la fiche %s : %s > %s",
 
 	/**
 	 * Form fields
 	 */
-	'groups:enableworkflow' => "Enable group workflow",
-	
+	'groups:enableworkflow' => "Activer les flux de travail",
+
 	'workflow:list:add_list' => "Ajouter une liste",
 	'workflow:list:add_card' => "Ajouter une fiche",
 
@@ -93,11 +94,11 @@ $french = array(
 	 */
 
 	'workflow:unknown_card' => "Fiche inconnue.",
-	
-	'workflow:board:add:success' => "Board saved.",
-	'workflow:board:save:failed' => "Board cannot be saved.",
-	'workflow:board:delete:success' => "Board successfully moved.",
-	'workflow:board:delete:failure' => "Delete board doesn't saved.",
+
+	'workflow:board:add:success' => "Tableau enregistré.",
+	'workflow:board:save:failed' => "Le tableau ne peut pas être enrigstré.",
+	'workflow:board:delete:success' => "Le tableau a bien été supprimé.",
+	'workflow:board:delete:failure' => "Le tableau n'a pas pu être sauvé.",
 
 	'workflow:list:move:failure' => "Erreur de connexion avec le serveur. Le déplacement de la liste n'a pas été enregistré.",
 	'workflow:list:move:success' => "La liste a bien été déplacé.",
@@ -124,21 +125,21 @@ $french = array(
 	 * Widget
 	 **/
 
-	
+
 	/**
 	 * Sidebar items
 	 */
 	 'workflow:sidebar:assignedto_user' => "Participants",
 	 'workflow:sidebar:assignedto_user:none' => "Pas de participant",
-	 'workflow:sidebar:last_activity_all_board' => "Last activity on all %s's boards",
-	 'workflow:sidebar:last_activity_on_this_board' => "Last activity on this board",
+	 'workflow:sidebar:last_activity_all_board' => "Dernière activité sur tous les tableaux de %s",
+	 'workflow:sidebar:last_activity_on_this_board' => "Dernières activités dans ce tableau",
 
 	/**
 	 * Settings
 	 */
 	'workflow:settings:min_list_column' => "Largeur des listes minimum",
 	'workflow:settings:max_list_column' => "Largeur des listes maximum",
-	'workflow:settings:defaut_max_list_column' => "Defaut value of maximum list's width is 242.667<br />Set empty to no limit.",
+	'workflow:settings:defaut_max_list_column' => "La largeur de liste maximum par défaut est 242.667<br />Mettez à zéro pour pas de limite.",
 );
 
 add_translation("fr", $french);
